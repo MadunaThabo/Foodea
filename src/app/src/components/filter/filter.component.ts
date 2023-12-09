2d16df8e4bb82d002618c1b99071639ad54885d4
Thabo Maduna <tmaduna@retrorabbit.co.za>
2023-12-09 10:08:58 +0200

    🚧 :Add search functionality and dynamic ingredient list

--- src/app/src/components/filter/filter.component.ts
+++ src/app/src/components/filter/filter.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
 export class FilterComponent {
 
+
+  ingredients: string[] = ['tomato', 'cheese', 'pepperoni', 'mushrooms', 'onions', 'olives', 'pineapple', 'ham', 'bacon', 'chicken', 'beef', 'sausage', 'peppers', 'spinach', 'jalapenos', 'anchovies'];
+  ingredientText: string = '';
+  constructor() {}
+
+  addIngredient() {
+    this.ingredients.push(this.ingredientText);
+    this.ingredientText = "";
+  }
+
+  removeIngredient(_t13: string) {
+    this.ingredients = this.ingredients.filter(t => t !== _t13);
+  }
 }
